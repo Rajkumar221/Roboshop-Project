d2506cd2b5107dc8b2fa439ee5d9eb2c82bfdb31
Rajkumar221 <rajkumarpatel52247@gmail.com>
2023-05-25 13:31:23 +0530

    change

---
+++ frontend.sh
@@ -0,0 +1,10 @@
+# roboshop nginx script
+yum install nginx -y 
+rm -rf /usr/share/nginx/html/* 
+curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip 
+cd /usr/share/nginx/html 
+unzip /tmp/frontend.zip
+vim /etc/nginx/default.d/roboshop.conf
+#we need to copy config file
+systemctl enable nginx 
+systemctl restart nginx 

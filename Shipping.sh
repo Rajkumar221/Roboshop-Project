2a317ace54796243472e1fab0e500a9d8a6be8d5
Rajkumar221 <rajkumarpatel52247@gmail.com>
2023-05-26 11:53:41 +0530

    setup shipping service

---
+++ Shipping.sh
@@ -0,0 +1,29 @@
+"\e[33m install maven\e[0m"
+yum install maven -y &>>/tmp/roboshop.log
+
+"\e[33m add user and app directory \e[0m"
+useradd roboshop &>>/tmp/roboshop.log
+mkdir /app 
+
+"\e[33m download the content \e[0m"
+curl -L -o /tmp/shipping.zip https://roboshop-artifacts.s3.amazonaws.com/shipping.zip  &>>/tmp/roboshop.log
+cd /app 
+unzip /tmp/shipping.zip
+
+"\e[33m extracat the file \e[0m"
+cd /app  &>>/tmp/roboshop.log
+mvn clean package 
+mv target/shipping-1.0.jar shipping.jar  &>>/tmp/roboshop.log
+
+"\e[33m reload \e[0m"
+systemctl daemon-reload &>>/tmp/roboshop.log
+
+"\e[33m install mysql client \e[0m"
+yum install mysql -y  &>>/tmp/roboshop.log
+
+"\e[33m load schema \e[0m"
+mysql -h <MYSQL-SERVER-IPADDRESS> -uroot -pRoboShop@1 < /app/schema/shipping.sql  &>>/tmp/roboshop.log
+
+"\e[33m start shipping service \e[0m"
+systemctl enable shipping &>>/tmp/roboshop.log
+systemctl restart shipping

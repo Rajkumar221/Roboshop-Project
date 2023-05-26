829a64512ca1a0014d7315754e72400503b74353
Rajkumar221 <rajkumarpatel52247@gmail.com>
2023-05-26 12:02:08 +0530

    rabbitmq service

---
+++ rabbitmq.sh
@@ -0,0 +1,17 @@
+"\e[33mConfigure YUM Repos\e[0m"
+curl -s https://packagecloud.io/install/repositories/rabbitmq/erlang/script.rpm.sh | bash &>>/tmp/roboshop.log
+
+"\e[33mConfigure YUM Repos for RabbitMQ.\e[0m"
+curl -s https://packagecloud.io/install/repositories/rabbitmq/rabbitmq-server/script.rpm.sh | bash &>>/tmp/roboshop.log
+
+"\e[33m install rabbitmq\e[0m"
+yum install rabbitmq-server -y  &>>/tmp/roboshop.log
+
+"\e[33mstart rabbitmq seerver\e[0m"
+systemctl enable rabbitmq-server  &>>/tmp/roboshop.log
+systemctl start rabbitmq-server 
+
+"\e[33m create user \e[0m"
+rabbitmqctl add_user roboshop roboshop123 &>>/tmp/roboshop.log
+rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*"
+

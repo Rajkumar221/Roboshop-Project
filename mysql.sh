44d8310ada23b901ca9949ef05362248633d93f3
Rajkumar221 <rajkumarpatel52247@gmail.com>
2023-05-26 12:43:24 +0530

    chane

--- mysql.sh
+++ mysql.sh
@@ -1,15 +1,15 @@
 echo -e "\e[33mdisable  defaul mysql \e[0m"
-yum module disable mysql -y 
+yum module disable mysql -y  &>>/tmp/roboshop.log
 
 echo -e "\e[33m setup mysql repo \e[0m"
-cp mysql.repo /etc/yum.repos.d/mysql.repo
+cp mysql.repo /etc/yum.repos.d/mysql.repo &>>/tmp/roboshop.log
 
 echo -e "\e[33m install mysql server\e[0m"
-yum install mysql-community-server -y
+yum install mysql-community-server -y &>>/tmp/roboshop.log
 
 echo -e "\e[33m set password \e[0m"
-mysql_secure_installation --set-root-pass RoboShop@1
+mysql_secure_installation --set-root-pass RoboShop@1 &>>/tmp/roboshop.log
 
 echo -e "\e[33m start mysql servicie \e[0m"
-systemctl enable mysqld
-systemctl start mysqld  
+systemctl enable mysqld &>>/tmp/roboshop.log
+systemctl start mysqld   &>>/tmp/roboshop.log

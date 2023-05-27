fc8aac8856148a3bc1c44d5599ede9fe614d4686
Rajkumar221 <rajkumarpatel52247@gmail.com>
2023-05-27 10:18:04 +0530

    payment

--- payment.sh
+++ payment.sh
@@ -5,6 +5,7 @@ echo -e "\e[33m add user \e[0m"
 useradd roboshop &>>/tmp/roboshop.log
 
 echo -e "\e[33m make app directory \e[0m"
+rm-f /app
 mkdir /app &>>/tmp/roboshop.log
  
 echo -e "\e[33m download the app content \e[0m"
@@ -17,7 +18,7 @@ cd /app  &>>/tmp/roboshop.log
 pip3.6 install -r requirements.txt &>>/tmp/roboshop.log
 
 echo -e "\e[33m setup systemd service \e[0m"
-cp /home/centos/roboshop-shell/payment.service etc/systemd/system/payment.service
+cp /home/centos/roboshop-shell/payment.service /etc/systemd/system/payment.service
 
 echo -e "\e[33m start payment service \e[0m"
 systemctl daemon-reload &>>/tmp/roboshop.log

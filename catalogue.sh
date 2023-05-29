b9d68e24d544c0b31a477359ca75b04cb9fd71c8
Rajkumar221 <rajkumarpatel52247@gmail.com>
2023-05-29 11:47:56 +0530

    variable

--- catalogue.sh
+++ catalogue.sh
@@ -4,9 +4,4 @@ component=catalogue
 
 nodejs
 
-echo -e "${color} Install mongodb shell ${nocolor}"
-cp mongodb.repo etc/yum.repos.d/mongodg.repo &>>$log_file
-yum install mongodb-org-shell -y &>>$log_file
-
-echo -e "${color} Load Schema ${nocolor}"
-mongo --host mongodb-dev.devops73.online <${app_path}/schema/$component.js &>>$log_file
+mongo_schema_setup
